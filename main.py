4631f9d10feee1722701f50197b6088c2d797268
ideamix <ideamix@ideamix.net>
2020-07-14 13:38:35 +0000

    00714

--- main.py
+++ main.py
@@ -1,1 +1,7 @@
-print("hihi")
+from indeed import extract_indeed_pages, extract_indeed_jobs
+
+last_indeed_page = extract_indeed_pages()
+
+indeed_jobs = extract_indeed_jobs(last_indeed_page)
+
+print(indeed_jobs)
